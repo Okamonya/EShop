8c8dcfb3ad580dc45007e113906344803e77c896
Oscar Kamonya <oscarkamonya@gmail.com>
2022-05-05 15:13:13 +0300

    First commit

---
+++ src/main/java/com/example/eshop/user/User.java
@@ -0,0 +1,84 @@
+package com.example.eshop.user;
+
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Getter
+@Setter
+@Entity
+@EqualsAndHashCode
+@AllArgsConstructor
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+    private Boolean enabled = false;
+    private Boolean locked = false;
+
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
---
+++ src/main/java/com/example/eshop/user/UserRole.java
@@ -0,0 +1,6 @@
+package com.example.eshop.user;
+
+public enum UserRole {
+    ADMIN,
+    USER
+}
